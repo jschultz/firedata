abbcdbfe359b7ac6eae65ec5c398eec2cb4aa961
Jonathan Schultz <jonathan@schultz.la>
2020-08-19 23:08:31 +0800

    Added Mondurup

--- extract_observations.sql
+++ extract_observations.sql
@@ -67,3 +67,17 @@ WHERE
     (("Station Name" = "ALBANY AIRPORT" OR "Station Name" = "ONGERUP")
     AND DateTime >= "2019-12-26" AND DateTime <= "2020-01-06")
 ORDER BY DateTime ASC ;
+
+.once Mondurup.csv
+SELECT stations."Station Number", "Station Name", "DateTime", 
+    "Precipitation in mm",
+    "Air temperature in Degrees C",
+    "Dew point temperature in Degrees C",
+    "Relative humidity in percentage %",
+    "Wind speed measured in km/h",
+    "Wind direction measured in degrees"
+FROM observations JOIN stations on observations."Station Number" = stations."Station Number" 
+WHERE
+    (("Station Name" = "ALBANY AIRPORT" OR "Station Name" = "ONGERUP")
+    AND DateTime >= "2019-12-06" AND DateTime <= "2019-12-11")
+ORDER BY DateTime ASC ;
