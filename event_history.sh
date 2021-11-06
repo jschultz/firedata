79bb7e4756f7a84a92a0d5bd3885b7ba2d9d552a
Jonathan Schultz <jonathan@schultz.la>
2021-11-06 10:26:45 +0800

    First re-work for FERA work

---
+++ event_history.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+#
+# Copyright 2021 Jonathan Schultz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+set -e
+
+help='Produces a list of events that intersect an area specified in one of several ways: burn ID, forest block'
+args=(
+# "-short:--long:variable:default:description:flags"
+  ":--debug:::Debug execution:flag"
+  "-u:--user:::PostgreSQL username:required"
+  "-d:--database:::PostgreSQL database:required"
+  "-v:--viewtable:::Name of table containing event history view:required"
+  "-a:--area:::Specification of area of which history will be extracted:required"
+  "-T:--threshhold::10:Minimum percentage of specified area for event to be included"
+  "-c:--viewcolumns::objectid:Semicolon-separated list of columns to retrieve from view data"
+  "-g:--viewgroups::objectid:Semicolon-separated list of columns to group view data"
+  ":--viewaliases:::Semicolon-separated list of aliases for columns retrieved from view data; empty value for no alias"
+  "-C:--csvfile:::CSV file to generate:output"
+  "-S:--shapefile:::Shapefile to generate:output"
+  "-l:--logfile:::Log file to record processing, defaults to out file name with extension replaced by '.log':private"
+  ":--nologfile:::Don't write a log file:private,flag"
+)
+
+source $(dirname "$0")/argparse.sh
+
+if [[ "${nologfile}" != "true" ]]; then
+    if [[ ! -n "${logfile}" ]]; then
+        if [[ -n "${shapefile}" ]]; then
+            logfile=$(basename ${shapefile})
+            logfile="${logfile%.*}.log"
+        elif [[ -n "${csvfile}" ]]; then
+            logfile=$(basename ${csvfile})
+            logfile="${logfile%.*}.log"
+        else
+            logfile="event_history.log"
+        fi
+    fi
+    if [[ -r "${logfile}" ]]; then
+        INCOMMENTS="$(< "${logfile}")"
+    else
+        INCOMMENTS=""
+    fi
+    echo "${COMMENTS}${INCOMMENTS}" > ${logfile}
+fi
+
+if [[ "${debug}" == "true" ]]; then
+    set -x
+fi
+
+IFS=';' read -r -a viewcolumnarray <<< "${viewcolumns}"
+IFS=';' read -r -a viewaliasarray <<< "${viewaliases}"
+IFS=';' read -r -a viewgrouparray <<< "${viewgroups}"
+
+HISTORY_QUERY="WITH area AS ${area} SELECT"
+separator=""
+for ((colidx=0; colidx<${#viewcolumnarray[@]}; colidx++)) do
+    HISTORY_QUERY+="${separator} ${viewcolumnarray[colidx]}"
+    if [[ -n "${viewaliasarray[colidx]}" ]]; then
+        HISTORY_QUERY+=" AS ${viewaliasarray[colidx]}"
+    fi
+    separator=","
+done
+HISTORY_QUERY+="
+    FROM ${viewtable} AS view
+WHERE
+    ST_Intersects(view.geom, ${area})"
+if [[ ${#viewgrouparray[@]} -gt 0 ]]; then
+    HISTORY_QUERY+="
+        GROUP BY"
+    separator=""
+    for ((colidx=0; colidx<${#viewgrouparray[@]}; colidx++)) do
+        HISTORY_QUERY+="${separator} ${viewgrouparray[colidx]}"
+        separator=","
+    done
+fi
+# echo $HISTORY_QUERY
+
+psql ${database} ${user} \
+    --quiet \
+    --command="$HISTORY_QUERY"
+
