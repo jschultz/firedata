8ea6ad0932ea2fc18f6d1c4dba548ef2f8a5ca1f
Jonathan Schultz <jonathan@schultz.la>
2021-12-29 20:26:37 +0800

    Script to produce atlas PDF for a single LMU

---
+++ exportLMUFireHistory.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020 Jonathan Schultz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from argrecord import ArgumentHelper, ArgumentRecorder
+from qgis.core import *
+from qgis.gui import QgsMapCanvas, QgsLayerTreeMapCanvasBridge
+from qgis.PyQt import QtGui
+from PyQt5.QtGui import QColor, QFont
+from PyQt5.QtCore import QDate, QTime, QDateTime
+from dateutil import parser as dateparser
+import subprocess
+import sys
+import csv
+
+def fireProgression(arglist=None):
+
+    parser = ArgumentRecorder(description='Present BOM data.',
+                              fromfile_prefix_chars='@')
+
+    parser.add_argument('-L', '--lmu',    type=str, help="LMU to export")
+    parser.add_argument('-l', '--layout', type=str, required=True, help="Print layout in QGIS file to export")
+    parser.add_argument('-o', '--outfile', type=str, help="Name of file to export, default to ?????")
+    
+    parser.add_argument('--logfile',      type=str, help="Logfile", private=True)
+    parser.add_argument('--no-logfile',   action='store_true', help='Do not output a logfile')
+    
+    parser.add_argument('qgisfile', type=str, nargs=1, help="Name of QGIS file to export")
+
+    args = parser.parse_args(arglist)
+
+    if not args.outfile:
+        args.outfile = args.lmu + '_' + args.layout + '.pdf'
+        
+    if not args.no_logfile:
+        if args.logfile:
+            logfilename = args.logfile
+        elif args.outfile:
+            logfilename = args.outfile.split('/')[-1].rsplit('.',1)[0] + '.log'
+                
+        logfile = open(logfilename, 'w')
+        parser.write_comments(args, logfile, incomments=ArgumentHelper.separator())
+        logfile.close()
+
+    QgsApplication.setPrefixPath("/usr", True)
+    qgs = QgsApplication([], True)
+    qgs.initQgis()
+
+    project = QgsProject.instance()
+    project.read(args.qgisfile[0])
+    
+    QgsExpressionContextUtils.setProjectVariable(project,'lmu',args.lmu)
+
+    manager = QgsProject.instance().layoutManager()
+    layout = manager.layoutByName(args.layout)
+    
+    exporter = QgsLayoutExporter(layout)
+    settings = QgsLayoutExporter.PdfExportSettings()
+    settings.simplifyGeometries = False
+    exporter.exportToPdf(layout.atlas(), args.outfile, settings)
+    
+    qgs.exitQgis()
+
+if __name__ == '__main__':
+    fireProgression(None)
