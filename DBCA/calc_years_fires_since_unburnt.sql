1667e8c3f2fee896e1e6ca16efb34887ebfa8bbb
Jonathan Schultz <jonathan@schultz.la>
2022-08-07 22:03:41 +0800

    Replace python with SQL

--- DBCA/calc_years_fires_since_unburnt.sql
+++ DBCA/calc_years_fires_since_unburnt.sql
@@ -1,41 +1,18 @@
-ALTER TABLE :table ADD COLUMN years_since_unburnt int, ADD COLUMN fires_since_unburnt int;
-SET ROLE dba;
-CREATE OR REPLACE FUNCTION calc_years_fires_since_unburnt(arg :table)
-  RETURNS :table
-  AS $$
-    fires=[]
-    idx = 1
-    while True:
-      fih_date1 = arg.get('fih_date1_'+str(idx))
-      if fih_date1:
-        if int(fih_date1[5:7]) >= 7:
-          season = int(fih_date1[0:4]) + 1
-        else:
-          season = int(fih_date1[0:4])
-
-        fires += [{'season': season, 'fih_fire_type': arg['fih_fire_type_'+str(idx)]}]
-      else:
-        break;
-      idx += 1
-      
-    threshold = 10
-    cur_year = 2021
-    idx = 0
-    arg['years_since_unburnt'] = None
-    arg['fires_since_unburnt'] = None
-    while idx < len(fires):
-      if cur_year - fires[idx]['season'] >= threshold:
-        arg['years_since_unburnt'] = 2021 - cur_year
-        arg['fires_since_unburnt'] = idx
-        break
-      else:
-        cur_year = fires[idx]['season']
-        idx += 1
-
-    return arg
-  $$ LANGUAGE plpython3u;
-RESET ROLE;
-CREATE TABLE temp2 AS
-(SELECT poly_id, (row).years_since_unburnt, (row).fires_since_unburnt 
-        FROM (SELECT poly_id, calc_years_fires_since_unburnt(:table) AS row 
-              FROM :table) AS foo) 
+SELECT geom, fslu, :refyear - season[fslu] AS yslu
+FROM (
+    SELECT 
+        poly_id,
+        season,
+        (SELECT ordinality from unnest(season[1:array_length(season,1)-1] @- season[2:]) WITH ORDINALITY WHERE unnest >= :mininterval limit 1) AS fslu
+    FROM (
+        SELECT poly_id, array_prepend(:refyear, array_agg(fireseason(fih_date1))) season 
+        FROM (
+            SELECT poly_id, history_id, fih_date1 
+            FROM dbca_fire_history_dbca_060_southern_history history, unnest(history.id) AS history_id, dbca_fire_history_dbca_060
+            WHERE dbca_fire_history_dbca_060.id = history_id
+            LIMIT 100
+          ) sub
+    GROUP BY poly_id
+    ) sub1
+) sub2
+LEFT JOIN dbca_fire_history_dbca_060_southern_poly poly on poly.id = poly_id
