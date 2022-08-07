796df23c8596fcc42c1c32be32dd8a929bfc4be4
Jonathan Schultz <jonathan@schultz.la>
2022-08-07 22:04:03 +0800

    More work

---
+++ DBCA/calc_fires_since_year.sql
@@ -0,0 +1,24 @@
+CREATE TABLE :table AS (
+    WITH ids AS (
+        SELECT
+            poly_id,
+            fireseason(fih_date1) AS thisfireseason,
+            fireseason(LEAD(fih_date1) OVER (PARTITION BY poly_id ORDER BY fih_date1 DESC)) AS prevfireseason
+        FROM
+            dbca_fire_history_dbca_060_southern_history history, 
+            unnest(history.id) AS history_id, 
+            dbca_fire_history_dbca_060
+        WHERE dbca_fire_history_dbca_060.id = history_id
+        AND fireseason(fih_date1) >= :refseason
+        ORDER BY fih_date1 DESC)
+    SELECT
+        poly_id, geom, numfires
+    FROM (
+        SELECT 
+            poly_id, 
+            (SELECT count(DISTINCT thisfireseason) FILTER(WHERE thisfireseason - prevfireseason > 1) AS numfires)
+        FROM 
+            ids
+        GROUP BY poly_id
+    ) sub
+    LEFT JOIN dbca_fire_history_dbca_060_southern_poly poly on poly.id = poly_id)
