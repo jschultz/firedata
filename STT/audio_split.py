d9d9473399b0d827f2a9c50988e8ddca66a8e81a
Jonathan Schultz <jonathan@schultz.la>
2023-12-16 22:31:17 +0800

    New script to split audio recordings into non-silent sections

---
+++ STT/audio_split.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2023 Jonathan Schultz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from argrecord import ArgumentHelper, ArgumentRecorder
+import subprocess
+import re
+import os
+from datetime import datetime, timedelta
+
+FFMPEG_BIN = "ffmpeg"
+
+def audioSplit(arglist=None):
+    parser = ArgumentRecorder(description='Split an audio file into non-silect sections',
+                              fromfile_prefix_chars='@')
+
+    parser.add_argument('-v', '--verbosity',  type=int, default=1, private=True)
+    
+    parser.add_argument('--logfile',      type=str, help="Logfile", private=True)
+    parser.add_argument('--nologfile',    action='store_true', help='Do not output a logfile')
+    parser.add_argument('--nobackup',     action='store_true', help='Do not back up existing output file')
+
+    parser.add_argument('infile',             type=str, help="Name of audio file to export", input=True)
+
+    args = parser.parse_args(arglist)
+
+    if not args.nologfile:
+        if args.logfile:
+            logfilename = args.logfile
+        else:
+            logfilename = 'audio_split.log'
+                
+        logfile = open(logfilename, 'w')
+        parser.write_comments(args, logfile, incomments=ArgumentHelper.separator())
+        logfile.close()
+
+    INFILE_REGEX = re.compile(R"(?P<prefix>.*)(?P<year>[0-9]{4})(?P<month>[0-9]{2})(?P<day>[0-9]{2})\-(?P<hour>[0-9]{2})(?P<minute>[0-9]{2})(?P<second>[0-9]{2}).*")
+    
+    infile_match = INFILE_REGEX.match(args.infile)
+    if infile_match:
+        baseprefix = infile_match.group('prefix')
+        basedatetime = datetime(int(infile_match.group('year')), int(infile_match.group('month')), int(infile_match.group('day')), int(infile_match.group('hour')), int(infile_match.group('minute')), int(infile_match.group('second')))
+        
+        print("basedatetime", basedatetime)
+        
+    command = [ FFMPEG_BIN, '-nostats',
+                '-i', args.infile,
+                '-af', 'silencedetect=duration=30:noise=0.1', '-f', 'null', 
+                '/dev/null']
+
+    pipe = subprocess.Popen(command, stderr=subprocess.PIPE)
+    
+    SILENCE_END_REGEX = re.compile(R".*silence_end: (?P<silence_end>[0-9]*(\.[0-9]*)?).*silence_duration: (?P<silence_duration>[0-9]*(\.[0-9]*)?)")
+    
+    sound_start = None
+    while True:
+        line = pipe.stderr.readline().decode()
+        if not line:
+            break
+        
+        silence_end_match = SILENCE_END_REGEX.match(line)
+        if silence_end_match:
+            silence_end      = float(silence_end_match.group('silence_end'))
+            silence_duration = float(silence_end_match.group('silence_duration'))
+            
+            silence_start = silence_end - silence_duration
+            
+            if sound_start:
+                datetime_start = basedatetime + timedelta(seconds = sound_start)
+                
+                replay_command = [ FFMPEG_BIN, '-nostats',
+                                '-i', args.infile,
+                                '-ss', str(sound_start),
+                                '-to', str(silence_start),
+                                '-c', 'copy',
+                                baseprefix + datetime_start.strftime("%Y%m%d-%H%M%S") + '.wav' ]
+            
+                subprocess.run(replay_command)
+                
+            sound_start = silence_end
+                            
+            
+    
+        
+if __name__ == '__main__':
+    audioSplit(None)
