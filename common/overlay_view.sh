796bef03535cd4a6f57a105882d01bf1130eea2f
Jonathan Schultz <jonathan@schultz.la>
2024-02-24 22:55:40 +0800

    Added flatten and limit options

--- common/overlay_view.sh
+++ common/overlay_view.sh
@@ -21,13 +21,16 @@ help='Produces a table or shapefile containing polygon columns and a selection o
 args=(
 # "-short:--long:variable:default:description:flags"
   ":--debug:::Debug execution:flag"
+  ":--verbosity::1:Verbosity level"
   "-e:--eventtable:::Semicolon-delimited name(s) of database table(s) containing event data":required
   ":--eventid:::Semicolon-delimited Id column(s) in event table(s). Default is 'id'"
-  ":--eventorder:::Semicolon-separated list of sort order for event table(s); empty value means use 'eventid'"
-  ":--eventlimit:::Semicolon-separated list of maximum number of events to retrieve from event table(s); empty valuemeans unlimited"
+  ":--eventorder:::Semicolon-separated list of sort order for event table(s)"
+  ":--eventfilter:::WHERE clause for selecting from event table(s)"
+  ":--eventlimit:::Semicolon-separated list of maximum number of events to retrieve from event table(s); empty means unlimited"
   "-b:--basename:::Table name base for output dump, polygon, point-in-polygon and junction output tables. Default is first event table name"
   "-j:--junction:::Semicolon-delimited junction table names. Default is 'eventtable'_junction"
   "-S:--suffix:::Suffix to append to first event table name to generate dump, polygon, point-in-polygon and junction table names:deprecated"
+  ":--polyfilter:::WHERE clause for selecting from polygon table"
   ":--polycolumns:::Semicolon-separated list of columns to retrieve from polygon table"
   ":--polyaliases:::Semicolon-separated list of aliases for columns retrieved from polygon table"
   "-c:--eventcolumns::id:Semicolon-separated list of fully specified columns (table and column name) to retrieve from event table(s)"
@@ -35,7 +38,6 @@ args=(
   ":--flatten:::Output event columns as separate columns instead of arrays:flag"
   ":--indexes:::Semicolon-separated list of indexes to create on view table"
   ":--using:::Semicolon-separated list of index methods"
-  "-w:--where:::WHERE clause for selecting from polygon table"
   "-a:--append:::Append output to existing view table:flag"
   "-v:--viewtable:::Table to generate, defaults to \$eventtable + \$suffix + '_view'"
   "-S:--viewfile:::Shapefile to generate:output"
@@ -51,17 +53,18 @@ if [[ "${debug}" == "true" ]]; then
     set -x
 fi
 
-IFS=';' read -r -a eventtable_array <<< "${eventtable}"
-IFS=';' read -r -a eventid_array    <<< "${eventid}"
-IFS=';' read -r -a eventorder_array <<< "${eventorder}"
-IFS=';' read -r -a eventlimit_array <<< "${eventlimit}"
-IFS=';' read -r -a junction_array   <<< "${junction}"
-IFS=';' read -r -a polycolumn_array <<< "${polycolumns}"
-IFS=';' read -r -a polyalias_array  <<< "${polyaliases}"
+IFS=';' read -r -a eventtable_array  <<< "${eventtable}"
+IFS=';' read -r -a eventid_array     <<< "${eventid}"
+IFS=';' read -r -a eventorder_array  <<< "${eventorder}"
+IFS=';' read -r -a eventfilter_array <<< "${eventfilter}"
+IFS=';' read -r -a eventlimit_array  <<< "${eventlimit}"
+IFS=';' read -r -a junction_array    <<< "${junction}"
+IFS=';' read -r -a polycolumn_array  <<< "${polycolumns}"
+IFS=';' read -r -a polyalias_array   <<< "${polyaliases}"
 IFS=';' read -r -a eventcolumn_array <<< "${eventcolumns}"
-IFS=';' read -r -a eventalias_array <<< "${eventaliases}"
-IFS=';' read -r -a index_array      <<< "${indexes}"
-IFS=';' read -r -a using_array      <<< "${using}"
+IFS=';' read -r -a eventalias_array  <<< "${eventaliases}"
+IFS=';' read -r -a index_array       <<< "${indexes}"
+IFS=';' read -r -a using_array       <<< "${using}"
 
 if [[ ! -n "${basename}" ]]; then
     basename=${eventtable_array[0]}
@@ -101,6 +104,12 @@ fi
 polytable=${basename}_poly
 
 for ((tableidx=0; tableidx<${#eventtable_array[@]}; tableidx++)) do
+    canonical_array[tableidx]=$(psql --variable=ON_ERROR_STOP=1 \
+        --quiet --tuples-only --no-align --command="\timing off" \
+        --command="SELECT canonical_table('${eventtable_array[tableidx]}')")
+    if [[ ! -n "${canonical_array[tableidx]}" ]]; then
+        canonical_array[tableidx]="${eventtable_array[tableidx]}"
+    fi
     if [[ ! -n "${eventid_array[tableidx]}" ]]; then
         eventid_array[tableidx]="id"
     fi
@@ -115,29 +124,52 @@ fi
 
 for ((colidx=0; colidx<${#eventcolumn_array[@]}; colidx++)) do
     if [[ ! -n "${eventalias_array[colidx]}" ]]; then
-        eventalias_array[colidx]="${eventcolumn_array[colidx]//./_}"
+        eventalias_array[colidx]="${eventcolumn_array[colidx]#*.}"
     fi
-    eventtype_array[colidx]=$(                                                   \
-        psql --quiet --tuples-only --no-align --command="\timing off"            \
-             --command="SELECT pg_typeof(${eventcolumn_array[colidx]}) FROM ${eventcolumn_array[colidx]%.*} LIMIT 1" \
+    TYPEOF_QUERY="SELECT pg_typeof(${eventcolumn_array[colidx]}) FROM"
+    separator=""
+    for ((tableidx=0; tableidx<${#eventtable_array[@]}; tableidx++)) do
+        TYPEOF_QUERY+="${separator} ${canonical_array[tableidx]} AS ${eventtable_array[tableidx]}"
+        separator=","
+    done
+    TYPEOF_QUERY+=" LIMIT 1"
+    if [[ ${verbosity} -ge 2 ]]; then
+        echo $TYPEOF_QUERY
+    fi
+    eventtype_array[colidx]=$(                                        \
+        psql --quiet --tuples-only --no-align --command="\timing off" \
+             --command="${TYPEOF_QUERY}"                              \
     )
 done
 
 for ((tableidx=0; tableidx<${#eventtable_array[@]}; tableidx++)) do
-    if [[ "${flatten}" = "true" && ! -n "${eventlimit_array[tableidx]}" ]]; then
+    if [[ "${flatten}" == "true" && ! -n "${eventlimit_array[tableidx]}" ]]; then
         LIMIT_QUERY="SELECT coalesce(max(count),0)"
         LIMIT_QUERY+=" FROM (SELECT count(*) AS count"
         LIMIT_QUERY+=" FROM ${junction_array[tableidx]}"
-        LIMIT_QUERY+=" JOIN ${eventtable_array[tableidx]} ON ${eventtable_array[tableidx]}.${eventid_array[tableidx]}=${junction_array[tableidx]}.${eventid_array[tableidx]}"
-        if [[ -n "${where}" ]]; then
+        LIMIT_QUERY+=" JOIN ${canonical_array[tableidx]} AS ${eventtable_array[tableidx]} ON ${eventtable_array[tableidx]}.${eventid_array[tableidx]}=${junction_array[tableidx]}.${eventid_array[tableidx]}"
+        if [[ -n "${polyfilter}" ]]; then
             LIMIT_QUERY+=" JOIN ${polytable} ON ${polytable}.id = poly_id"
-            LIMIT_QUERY+=" WHERE ${where}"
+            LIMIT_QUERY+=" WHERE (${polyfilter})"
+        fi
+        if [[ -n "${eventfilter[tableidx]}" ]]; then
+            if [[ -n "${polyfilter}" ]]; then
+                LIMIT_QUERY+=" AND (${eventfilter[tableidx]})"
+            else
+                LIMIT_QUERY+=" WHERE (${eventfilter[tableidx]})"
+            fi
         fi
         LIMIT_QUERY+=" GROUP BY ${junction_array[tableidx]}.poly_id) AS foo"
+        if [[ ${verbosity} -ge 2 ]]; then
+            echo $LIMIT_QUERY
+        fi
         eventlimit_array[tableidx]=$(psql \
                 --quiet --tuples-only --no-align \
                 --command="\timing off" \
                 --command="${LIMIT_QUERY}")
+        if [[ ${verbosity} -ge 1 ]]; then
+            echo Limit for event table ${eventtable_array[tableidx]} is ${eventlimit_array[tableidx]}
+        fi
     fi
 done
 
@@ -167,46 +199,52 @@ for ((tableidx=0; tableidx<${#eventtable_array[@]}; tableidx++)) do
         VIEW_QUERY+=" LATERAL"
     fi
     VIEW_QUERY+=" (SELECT"
-    if [[ ! -n "${eventlimit_array[tableidx]}" ]]; then
-        VIEW_QUERY+=" ${eventtable_array[tableidx]}.poly_id,"
-    fi
     separator=""
+    if [[ ! -n "${eventlimit_array[tableidx]}" ]]; then
+        VIEW_QUERY+=" ${eventtable_array[tableidx]}.poly_id"
+        separator=","
+    fi
     for ((colidx=0; colidx<${#eventcolumn_array[@]}; colidx++)) do
         if [[ "${eventcolumn_array[colidx]%.*}" == "${eventtable_array[tableidx]}" ]]; then
-            if [[ "${flatten}" != "true" ]]; then
+            if [[ "${flatten}" == "true" ]]; then
+                for ((linkidx=1; linkidx<=${eventlimit_array[colidx]}; linkidx++)) do
+                    VIEW_QUERY+="${separator} (array_agg(${eventcolumn_array[colidx]}))[${linkidx}] AS \"${eventcolumn_array[colidx]#*.}_${linkidx}\""
+                    separator=","
+                done
+            else
                 VIEW_QUERY+="${separator} array_agg(${eventcolumn_array[colidx]}"
                 if [[ -n "${eventorder_array[tableidx]}" ]]; then
                     VIEW_QUERY+=" ORDER BY ${eventorder_array[tableidx]}"
                 fi
                 VIEW_QUERY+=") AS \"${eventcolumn_array[colidx]#*.}\""
                 separator=","
-            else
-                for ((linkidx=1; linkidx<=${eventlimit_array[colidx]}; linkidx++)) do
-                    VIEW_QUERY+="${separator} (array_agg(${eventcolumn_array[colidx]}))[${linkidx}] AS \"${eventcolumn_array[colidx]#*.}_${linkidx}\""
-                    separator=","
-                done
             fi
         fi
     done
-    VIEW_QUERY+=" FROM (SELECT ${junction_array[tableidx]}.poly_id,"
-    separator=""
+    VIEW_QUERY+=" FROM (SELECT ${junction_array[tableidx]}.poly_id"
     for ((colidx=0; colidx<${#eventcolumn_array[@]}; colidx++)) do
         if [[ "${eventcolumn_array[colidx]%.*}" == "${eventtable_array[tableidx]}" ]]; then
-            VIEW_QUERY+="${separator} ${eventcolumn_array[colidx]}"
-            separator=","
+            VIEW_QUERY+=", ${eventcolumn_array[colidx]}"
         fi
     done
-    VIEW_QUERY+=" FROM ${junction_array[tableidx]}, ${eventtable_array[tableidx]}"
-    VIEW_QUERY+=" WHERE ${eventtable_array[tableidx]}.${eventid_array[tableidx]}=${junction_array[tableidx]}.${eventid_array[tableidx]}"
+    VIEW_QUERY+=" FROM ${junction_array[tableidx]}"
+    VIEW_QUERY+=" JOIN ${canonical_array[tableidx]} AS ${eventtable_array[tableidx]}"
+    VIEW_QUERY+=" ON ${eventtable_array[tableidx]}.${eventid_array[tableidx]}=${junction_array[tableidx]}.${eventid_array[tableidx]}"
+    separator="WHERE"
     if [[ -n "${eventlimit_array[tableidx]}" ]]; then
-        VIEW_QUERY+=" AND ${junction_array[tableidx]}.poly_id = ${polytable}.id"
+        VIEW_QUERY+=" ${separator} ${junction_array[tableidx]}.poly_id = ${polytable}.id"
+        separator="AND"
     fi
-    if [[ -n "${eventorder_array[tableidx]}" ]]; then
-        VIEW_QUERY+=" ORDER BY ${eventorder_array[tableidx]}"
+    if [[ -n "${eventfilter_array[tableidx]}" ]]; then
+        VIEW_QUERY+=" ${separator} (${eventfilter_array[tableidx]})"
+        separator="AND"
     fi
-#     if [[ -n "${eventlimit_array[tableidx]}" ]]; then
-#         VIEW_QUERY+=" LIMIT ${eventlimit_array[tableidx]}"
-#     fi
+    if [[ -n "${eventlimit_array[tableidx]}" ]]; then
+        if [[ -n "${eventorder_array[tableidx]}" ]]; then
+            VIEW_QUERY+=" ORDER BY ${eventorder_array[tableidx]}"
+        fi
+        VIEW_QUERY+=" LIMIT ${eventlimit_array[tableidx]}"
+    fi
     VIEW_QUERY+=") AS ${eventtable_array[tableidx]}"
     VIEW_QUERY+=" GROUP BY ${eventtable_array[tableidx]}.poly_id"
     VIEW_QUERY+=") AS ${eventtable_array[tableidx]}"
@@ -216,8 +254,8 @@ for ((tableidx=0; tableidx<${#eventtable_array[@]}; tableidx++)) do
         VIEW_QUERY+=" ON ${eventtable_array[tableidx]}.poly_id = ${polytable}.id"
     fi
 done
-if [[ -n "${where}" ]]; then
-    VIEW_QUERY+=" WHERE ${where}"
+if [[ -n "${polyfilter}" ]]; then
+    VIEW_QUERY+=" WHERE (${polyfilter})"
 fi
 if [[ ${#polycolumn_array[@]} -gt 0 ]]; then
     VIEW_QUERY+=" GROUP BY"
@@ -239,19 +277,21 @@ if [[ ${#polycolumn_array[@]} -gt 0 ]]; then
     done
 fi
 
-if [[ "${debug}" == "true" ]]; then
-    echo "---------------------------------------------" > /dev/stderr
-    echo "$VIEW_QUERY"                                   > /dev/stderr  
-    echo "---------------------------------------------" > /dev/stderr
+if [[ ${verbosity} -ge 2 ]]; then
+    echo "$VIEW_QUERY" > /dev/stderr  
 fi
 
 if [[ -n "${viewfile}" ]]; then
-    echo "Creating shapefile ${viewfile}"
+    if [[ ${verbosity} -ge 1 ]]; then
+        echo "Creating shapefile ${viewfile}"
+    fi
     pgsql2shp -q -f ${viewfile} -u $PGUSER $PGDATABASE "${VIEW_QUERY}"
     zip --move --junk-paths "${viewfile}".zip "${viewfile}".{cpg,dbf,prj,shp,shx}
 else
     if [[ "${append}" != "true" ]]; then
-        echo "Creating table ${viewtable}"
+        if [[ ${verbosity} -ge 1 ]]; then
+            echo "Creating table ${viewtable}"
+        fi
         if [[ "${nobackup}" != "true" ]]; then  
             backupcommand="CALL cycle_table('${viewtable}')"
         else
@@ -276,7 +316,9 @@ else
                  --command="CREATE INDEX ON ${viewtable} ${using} (${index_array[indexidx]})"
         done
     else
-        echo "Appending to table ${viewtable}"
+        if [[ ${verbosity} -ge 1 ]]; then
+            echo "Appending to table ${viewtable}"
+        fi
         if [[ "${nocomments}" != "true" ]]; then
             INCOMMENTS=$(psql --csv --tuples-only --no-align --quiet --command="\timing off" --command "SELECT obj_description('${viewtable}'::regclass, 'pg_class')")
             NEWCOMMENTS="${COMMENTS}${INCOMMENTS}"
