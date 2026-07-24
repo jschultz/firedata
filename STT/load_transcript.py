de7723775c4db3b94dfcb671a4b59237e325b229
Jonathan Schultz <jonathan@schultz.la>
2026-07-24 12:44:36 +0800

    Improved lrc file processing

--- STT/load_transcript.py
+++ STT/load_transcript.py
@@ -34,6 +34,8 @@ def loadTranscript(arglist=None):
 
     parser.add_argument('-c', '--csvfile',    type=str,
                                               help='Output CSV file', output=True)
+    parser.add_argument('-l', '--lrcfile',    type=str,
+                                              help='Output LRC file', output=True)
     parser.add_argument('-d', '--database',   type=str,
                                               help='SQLAlchemy database specification')
 
@@ -59,6 +61,11 @@ def loadTranscript(arglist=None):
         csvwriter=csv.DictWriter(csvfile, fieldnames=['name', 'channel', 'datetime', 'text'])
         if not args.no_header:
             csvwriter.writeheader()
+
+    if args.lrcfile:
+        if os.path.exists(args.lrcfile):
+            shutil.move(args.lrcfile, args.lrcfile + '.bak')
+        lrcfile = open(args.lrcfile, 'w')
 
     if args.database:
         if not args.no_logfile:
@@ -87,8 +94,8 @@ def loadTranscript(arglist=None):
 
         transaction = connection.begin()
 
-    filenameregexp = re.compile(R"(?P<name>.+?)(-Chan(?P<channel>[0-9]+))?-(?P<year>[0-9]{2,4})(?P<month>[0-9]{2})(?P<day>[0-9]{2})-(?P<hour>[0-9]{2})(?P<minute>[0-9]{2})(?P<second>[0-9]{2}).+", re.UNICODE)
-    lineregexp = re.compile(r"^\[(?P<minute>[0-9]{2}):(?P<second>[0-9]{2}).(?P<csec>[0-9]{2})\]\s*(?P<text>.*)$", re.UNICODE)
+    filenameregexp = re.compile(R"(?P<name>[^-]+)(-Chan(?P<channel>[0-9]+))?(-(?P<year>[0-9]{2,4})(?P<month>[0-9]{2})(?P<day>[0-9]{2}))?(-(?P<hour>[0-9]{2})(?P<minute>[0-9]{2})(?P<second>[0-9]{2}))?.+", re.UNICODE)
+    lineregexp = re.compile(r"^\[(?P<minute>[0-9]{1,3}):(?P<second>[0-9]{2}).(?P<csec>[0-9]{2})\]\s*(?P<text>.*)$", re.UNICODE)
 
     files = []
     for filename in glob.glob(args.inpattern):
@@ -96,18 +103,23 @@ def loadTranscript(arglist=None):
         if filenamematch:
             name      = filenamematch.group('name')
             channel   = int(filenamematch.group('channel') or 0)
-            year      = int(filenamematch.group('year'))
-            if year < 100:
+            year      = int(filenamematch.group('year') or 0)
+            if year and year < 100:
                 year += 2000
-            month     = int(filenamematch.group('month'))
-            day       = int(filenamematch.group('day'))
-            hour      = int(filenamematch.group('hour'))
-            minute    = int(filenamematch.group('minute'))
-            second    = int(filenamematch.group('second'))
+            month     = int(filenamematch.group('month') or 0)
+            day       = int(filenamematch.group('day') or 0)
+            hour      = int(filenamematch.group('hour') or 0)
+            minute    = int(filenamematch.group('minute') or 0)
+            second    = int(filenamematch.group('second') or 0)
 
-            files.append( {'fullname': filename, 'name': name, 'channel': channel, 'basetime': datetime.datetime(year, month, day, hour, minute, second)} )
+            if year and month:
+                basetime = datetime.datetime(year, month, day, hour, minute, second)
+            else:
+                basetime = None
+
+            files.append( {'fullname': filename, 'name': name, 'channel': channel, 'basetime': basetime} )
         else:
-            print("ERROR: File " + filename + " does not match pattern", file=sys.stderr)
+            print("ERROR: Filename " + filename + " does not match pattern", file=sys.stderr)
 
 
     for fileinfo in sorted(files, key=lambda fileinfo: fileinfo['basetime']):
@@ -118,6 +130,8 @@ def loadTranscript(arglist=None):
             linematch = lineregexp.match(line)
             if linematch:
                 minute = int(linematch.group('minute'))
+                hour = minute // 60
+                minute = minute % 60
                 second = int(linematch.group('second'))
                 csec   = int(linematch.group('csec'))
                 text   =     linematch.group('text')
@@ -125,10 +139,16 @@ def loadTranscript(arglist=None):
                 if text != lasttext:
                     lasttext = text
 
-                    finaltime = fileinfo['basetime'] + datetime.timedelta(minutes=minute, seconds=second, milliseconds = csec*10)
+                    basetime = fileinfo['basetime'] or 0
+                    if basetime:
+                        finaltime = basetime + datetime.timedelta(hours=hour, minutes=minute, seconds=second, milliseconds = csec*10)
+                    else:
+                        finaltime = datetime.time(hour=hour, minute=minute, second=second, microsecond = csec*10000)
 
                     if args.csvfile:
                         csvwriter.writerow({'name':fileinfo['name'], 'channel':fileinfo['channel'], 'datetime':finaltime, 'text':text})
+                    if args.lrcfile:
+                        print(f"[{finaltime.hour*60+finaltime.minute}:{finaltime.second:02}:{finaltime.microsecond//10000:02}] {text}", file=lrcfile)
                     if args.database:
                         connection.execute(transcript.delete().where(transcript.c.DateTime == finaltime))
                         connection.execute(transcript.insert().values({
@@ -139,6 +159,8 @@ def loadTranscript(arglist=None):
 
     if args.csvfile:
         csvfile.close()
+    if args.lrcfile:
+        lrcfile.close()
     if args.database:
         transaction.commit()
         connection.close()
